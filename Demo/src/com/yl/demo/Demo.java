dc417f8ae3190626cbb8182ab1d220b231c03de6
alex <824187670@qq.com>
2018-06-11 15:33:28 +0800

    demo

--- Demo/src/com/yl/demo/Demo.java
+++ Demo/src/com/yl/demo/Demo.java
@@ -1,11 +1,16 @@
 package com.yl.demo;
 
 public class Demo {
-	
+
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		System.out.println(111);
-		
+
+		System.out.println("12345");
+
 	}
 	
 }
